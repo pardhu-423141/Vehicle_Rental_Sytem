aae4948144b8225174afe950682720f2ae996a3b
pardhu-423141 <pardhukatakam25@gmail.com>
2026-06-30 22:17:33 +0530

    updated axios

--- vehicle-rental-frontend/src/api/axios.ts
+++ vehicle-rental-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const api = axios.create({
-  baseURL: `${API_URL}/api` || 'http://localhost:5000/api',
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
